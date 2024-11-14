96e1665e5250628354204495c355e2410a5e03b9
trbouma <trbouma@gmail.com>
2024-11-14 17:49:04 -0500

    remove redundance function

--- safebox/wallet.py
+++ safebox/wallet.py
@@ -335,6 +335,7 @@ class Wallet:
         out_string += "\n"+ "-"*80  
         out_string += f"\nnpub: {str(self.pubkey_bech32)}"
         out_string += f"\npubhex: {str(self.pubkey_hex)}"
+        out_string += f"\nprivhex: {str(self.privkey_hex)}"
         out_string += f"\nnsec: {str(self.k.private_key_bech32())}"
         out_string += f"\n\nseed phrase: \n{'-'*80}\n{mnemo.to_mnemonic(bytes.fromhex(self.privkey_hex))}"
         out_string += "\n"+ "-"*80    
